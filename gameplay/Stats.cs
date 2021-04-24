0b7e91fd7b67551f4726786ef84d5c6f0cb49c28
Joel Johnson <johnson.joel.86@gmail.com>
2021-04-24 11:51:51 -0600

    labels display damage dealt over the character's head who takes the damage

--- gameplay/Stats.cs
+++ gameplay/Stats.cs
@@ -2,6 +2,8 @@ using System;
 using Godot;
 
 public class Stats : Node2D, IStats {
+    private PackedScene _fadingLabelScene;
+    
     [Export(PropertyHint.Range, "10,255")] public int MaxHealth { get; set; } = 10;
     public int RemainingHealth { get; set; } = 10;
 
@@ -14,19 +16,33 @@ public class Stats : Node2D, IStats {
     public IRoot Root { get; private set; }
     
     public override void _Ready() {
+        _fadingLabelScene = ResourceLoader.Load<PackedScene>($"res://gameplay/TemporaryLabel.tscn");
         RemainingHealth = MaxHealth;
-
         Root = this.FindRoot();
     }
 
     public void Heal(int amount) {
         RemainingHealth = Mathf.Clamp(RemainingHealth + amount, 0, MaxHealth);
         GD.Print(Root.Name(), " healed by ", amount, " -> ", RemainingHealth);
+
+        var label = _fadingLabelScene.Instance<TemporaryLabel>();
+        label.Text = $"+{amount}";
+        label.TextColor = Colors.Cyan;
+        label.Effect = TemporaryLabel.TextEffect.Wave;
+        label.GlobalPosition = Root.AsNode().GlobalPosition;
+        Root.AsNode().GetParent().AddChild(label);
     }
 
     public void Hurt(int amount) {
         RemainingHealth = Mathf.Clamp(RemainingHealth - amount, 0, MaxHealth);
         GD.Print(Root.Name(), " hurt by ", amount, " -> ", RemainingHealth);
+        
+        var label = _fadingLabelScene.Instance<TemporaryLabel>();
+        label.Text = $"-{amount}";
+        label.TextColor = Colors.Red;
+        label.Effect = TemporaryLabel.TextEffect.Shake;
+        label.GlobalPosition = Root.AsNode().GlobalPosition;
+        Root.AsNode().GetParent().AddChild(label);
 
         if (RemainingHealth <= 0) {
             Root.Die();
